21e80cd7be47d02aa692a2c48fd5889b0e95451d
alvin0319 <32565818+alvin0319@users.noreply.github.com>
2018-08-22 21:13:49 +0900

    Remove useless code

--- CreateZombie.php
+++ CreateZombie.php
@@ -31,9 +31,6 @@ use pocketmine\utils\Config;
 class CreateZombie extends PluginBase implements Listener {
 	public function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents ($this, $this);
-		//$this->cmd = new \pocketmine\command\PluginCommand("보스", $this);
-		//$this->cmd->setDescription ("Boss Command");
-		//$this->getServer()->getCommandMap()->register ("보스", $this->cmd);
 		if (! $this->getServer()->isLevelGenerated("BossLevel")) $this->getServer()->generateLevel("BossLevel",null,"FLAT");
         if (! $this->getServer()->isLevelLoaded("BossLevel")) $this->getServer()->loadLevel("BossLevel");
 		$this->getScheduler()->scheduleRepeatingTask (new class ($this) extends Task {
@@ -83,40 +80,4 @@ class CreateZombie extends PluginBase implements Listener {
             }
         }
     }
-    /**
-    public function onCommand(\pocketmine\command\CommandSender $sender, \pocketmine\command\Command $command, string $label, array $args) : bool{
-    	if ($command->getName() === "보스") {
-    	    if (! isset ($args[0])) {
-    	        $sender->sendMessage ("/보스 소환");
-                return true;
-            }
-            if ($args[0] === "소환") {
-            	if (! $sender->isOp()) {
-            	return true;
-                }
-                $nbt = new CompoundTag('', [
-    'Pos' => new ListTag('Pos', [
-        new DoubleTag('', 253),
-        new DoubleTag('', 4),
-        new DoubleTag('', 253)
-    ]),
-    'Motion' => new ListTag('Motion', [
-        new DoubleTag('', 0),
-        new DoubleTag('', 0),
-        new DoubleTag('', 0)
-    ]),
-    'Rotation' => new ListTag('Rotation', [
-        new FloatTag('', lcg_value() * 360),
-        new FloatTag('', 0)
-    ]),
-]);
-        $entity = Entity::createEntity(Entity::ZOMBIE, $this->getServer()->getLevelByName("BossLevel")->getSafeSpawn(), $nbt);    
-        $entity->setHealth(100);
-        $entity->spawnToAll();
-        $this->getServer()->broadcastMessage ("§b§l[ Boss ] §f보스가 스폰되었습니다");
-        }
-    }
-    return true;
-}
-*/
 }
